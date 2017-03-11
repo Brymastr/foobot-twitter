e013a4aa833f868c4f7b1348bd09063532009308
Brycen <dorsay@live.ca>
2017-03-10 16:34:29 -0800

    Schedule tweet moved to twitter module

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 const 
-  schedule = require('node-schedule'),
-  twitter = require('./twitter'),
-  strings = require('./strings'),
-  async = require('async'),
-  config = require('./config')();
+  twitter = require('./twitter');
 
-// Twitter
-schedule.scheduleJob({
-  hour: 10,
-  minute: 30
-}, () => {
-  async.retry({
-    times: 3,
-    interval: 2000,
-    errorFilter: err => {
-      return err === 187
-    }
-  }, (cb, results) => {
-    twitter.sendTweet(config, strings.$('tweet'))
-      .then(tweet => {
-        console.log('NEW TWEET:' + tweet);
-        cb(tweet);
-      })
-      .catch(err => {
-        cb(new Error(err));
-      });
-  });
-});
+twitter.scheduleTweet();
--- twitter.js
+++ twitter.js
@@ -1,5 +1,9 @@
 const
-  Twitter = require('twitter');
+  Twitter = require('twitter'),
+  schedule = require('node-schedule'),
+  strings = require('./strings'),
+  config = require('./config')(),
+  async = require('async');
 
 /**
  * Connect to a twitter stream. Sends an HTTP GET to the twitter api and returns a stream object
@@ -20,22 +24,39 @@ exports.connectStream = (config) => {
  * @param {String} status the content to be tweeted
  * @return {Promise} returns a new promise with tweet confirmation or error
  */
-exports.sendTweet = (config, status) => {
+exports.sendTweet = status => {
   return new Promise((resolve, reject) => {
-    var twitter = new Twitter({
-      consumer_key: config.twitter_api_key,
-      consumer_secret: config.twitter_api_secret,
-      access_token_key: config.twitter_access_token_key,
-      access_token_secret: config.twitter_access_token_secret
+    new Twitter({
+      consumer_key: config.twitterapi_key,
+      consumer_secret: config.twitterapi_secret,
+      access_token_key: config.twitteraccess_token_key,
+      access_token_secret: config.twitteraccess_token_secret
+    })
+    .post('statuses/update', {status})
+    .then(tweet => resolve(tweet.text))
+    .catch(err => {
+      if(err[0].code == 187) reject(187);
+      else reject(err);
     });
-    twitter
-      .post('statuses/update', {status})
-      .then(tweet => {
-        resolve(tweet.text);
-      })
-      .catch(err => {
-        if(err[0].code == 187) reject(187);
-        else reject(err);
-      });
+  });
+};
+
+exports.scheduleTweet = () => {
+  let rule = new schedule.RecurrenceRule();
+  rule.hour = 10;
+  rule.minute = 30;
+  schedule.scheduleJob(rule, () => {
+    async.retry({
+      times: 5,
+      interval: 2000,
+      errorFilter: err => err === 187
+    }, (cb, results) => {
+      this.sendTweet(require('../strings').$('tweet'))
+        .then(tweet => {
+          log.info(`Tweet sent: ${tweet}`);
+          cb(tweet);
+        })
+        .catch(err => cb(new Error(err)));
+    });
   });
 };
