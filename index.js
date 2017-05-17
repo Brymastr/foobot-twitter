ae068c83d92bd5c1af0fcdd33106a84845d02516
Brycen <dorsay@live.ca>
2017-05-17 12:56:02 -0700

    10:30

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sendTweet(status) {
     .post('statuses/update', {status})
     .then(tweet => resolve(tweet.text))
     .catch(err => {
-      if(err[0].code == 187) reject(187);
+      if(err[0].code === 187) reject(187);
       else reject(err);
     });
   });
@@ -31,8 +31,8 @@ function sendTweet(status) {
 
 async function scheduleTweet() {
   let rule = new schedule.RecurrenceRule();
-  // rule.hour = 10;
-  // rule.minute = 30;
+  rule.hour = 10;
+  rule.minute = 30;
 
   schedule.scheduleJob(rule, async () => {
     let tweet = strings.$('tweet');
@@ -43,6 +43,7 @@ async function scheduleTweet() {
     } catch(err) {
       console.log(err);
     }
+
   });
 }
 
